fe5ee3cd2d72546e4639b91895a45af4708d49bc
Bartek5765 <bprzeczek@student.agh.edu.pl>
2025-05-11 12:16:07 +0200

    zmiany

--- src/main/frontend/src/meetings/MeetingsList.js
+++ src/main/frontend/src/meetings/MeetingsList.js
@@ -1,10 +1,12 @@
-export default function MeetingsList({meetings}) {
+export default function MeetingsList({meetings, onDelete}) {
     return (
         <table>
             <thead>
             <tr>
                 <th>Nazwa spotkania</th>
                 <th>Opis</th>
+                <th>Akcje</th>
+
             </tr>
             </thead>
             <tbody>
@@ -12,6 +14,11 @@ export default function MeetingsList({meetings}) {
                 meetings.map((meeting, index) => <tr key={index}>
                     <td>{meeting.title}</td>
                     <td>{meeting.description}</td>
+                    <td>
+                        <button type="button"
+                                className="button-out-line"
+                                onClick={() => onDelete(meeting)}>Usuń</button>
+                    </td>
                 </tr>)
             }
             </tbody>
--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -13,6 +13,7 @@ export default function MeetingsPage({username}) {
             headers: { 'Content-Type': 'application/json' }
         });
         if (response.ok) {
+            const newMeeting = await response.json();
             const nextMeetings = [...meetings, meeting];
             setMeetings(nextMeetings);
             setAddingNewMeeting(false);
@@ -30,9 +31,14 @@ export default function MeetingsPage({username}) {
         fetchMeetings();
     }, []);
 
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
+    async function handleDeleteMeeting(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}`, {
+            method: 'DELETE',
+        });
+        if (response.ok) {
+            const nextMeetings = meetings.filter(m => m !== meeting);
+            setMeetings(nextMeetings);
+        }
     }
 
     return (
