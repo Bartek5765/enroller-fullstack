ab369d11ed1b2901f2a375af7ca4432d5f36d291
Bartek5765 <bprzeczek@student.agh.edu.pl>
2025-05-25 13:52:50 +0200

    zmiany

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -34,6 +34,7 @@ export default function MeetingsPage({username}) {
     async function handleDeleteMeeting(meeting) {
         const response = await fetch(`/api/meetings/${meeting.id}`, {
             method: 'DELETE',
+
         });
         if (response.ok) {
             const nextMeetings = meetings.filter(m => m !== meeting);
